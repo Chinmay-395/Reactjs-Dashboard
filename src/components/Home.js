2f6b6622673709d5391d50261f2600075efc4991
Chinmay-395 <chinmaydali395@gmail.com>
2021-03-06 10:59:56 +0530

    completed the skeleton of the website and adding custom css

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,35 +14,44 @@ export const Home = (props) => (
     <>
       <div className="container">
         {/* First row with LESSONS TODOs & LEADERBOARD */}
-        <div className="row">
-          <div class="col">
+        <div style={{ marginBottom: "1rem" }} className="row">
+          <div className="col">
             {/* column 1 for Lessons */}
-            <div class="card">
-              <div class="card-body">
+            <div className="card">
+              <div className="card-header">UPCOMMING LESSONS</div>
+              <div className="card-body">
+                {/* <h5 className="card-title"></h5> */}
                 <div className="row">row 1</div>
                 <div className="row">row 2</div>
                 <div className="row">row 3</div>
               </div>
+              <div className="card-footer">Footer</div>
             </div>
           </div>
-          <div class="col">
+          <div className="col">
             {/* cloumn 2 ToDos */}
-            <div class="card">
-              <div class="card-body">
+            <div className="card">
+              <div className="card-header">TODOs</div>
+              <div className="card-body">
+                {/* <h5 className="card-title">TODOs</h5> */}
                 <div className="row">row 1</div>
                 <div className="row">row 2</div>
                 <div className="row">row 3</div>
               </div>
+              <div className="card-footer">Footer</div>
             </div>
           </div>
-          <div class="col">
+          <div className="col">
             {/* Column 3 Leaderboard */}
-            <div class="card">
-              <div class="card-body">
+            <div className="card">
+              <div className="card-header">LEADERBOARD</div>
+              <div className="card-body">
+                {/* <h5 className="card-title">LEADERBOARD</h5> */}
                 <div className="row">row 1</div>
                 <div className="row">row 2</div>
                 <div className="row">row 3</div>
               </div>
+              <div className="card-footer">Footer</div>
             </div>
           </div>
         </div>
@@ -51,11 +60,13 @@ export const Home = (props) => (
           {/* 2/3 would be reserved for My-courses row and Graph row */}
           <div className="col-9">
             <div className="card">
-              <div class="card-body">
+              <div className="card-body">
                 {/* Row 1 will use for My-Course */}
                 <div className="row">
-                  <div class="card">
-                    <div class="card-body">
+                  <div className="card">
+                    <div className="card-header">MY COURSES</div>
+                    <div className="card-body">
+                      {/* <h5 className="card-title">MY COURSES</h5> */}
                       This card body is for <strong>My courses</strong>
                       xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.
                     </div>
@@ -63,8 +74,10 @@ export const Home = (props) => (
                 </div>
                 {/* Row 2 will use for Graph */}
                 <div className="row">
-                  <div class="card">
-                    <div class="card-body">
+                  <div className="card">
+                    <div className="card-header">GRAPHS</div>
+                    <div className="card-body">
+                      {/* <h5 className="card-title">GRAPHS</h5> */}
                       This card body is for <strong>Graphs</strong>
                       xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.
                     </div>
@@ -76,7 +89,11 @@ export const Home = (props) => (
           {/* 1/3 would be reserved for Homework progess */}
           <div className="col-3">
             <div className="card">
-              <div class="card-body">HomeWork progress</div>
+              <div className="card-header">HOMEWORK</div>
+              <div className="card-body">
+                <h5 className="card-title">HOMEWORK</h5>
+                HomeWork progress
+              </div>
             </div>
           </div>
         </div>
